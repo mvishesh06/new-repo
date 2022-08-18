62a29785c1e3ef21147f8265c454974bbbb1a0ab
unknown <visheshjimishra@gmail.com>
2022-08-18 16:08:25 +0530

    added

--- providers.tf
+++ providers.tf
@@ -8,5 +8,7 @@ terraform {
     bucket = "javahome-tf-1212"
     key = "terraform.tfstate"
     region = "us-east-1"
+    access_key = "AKIA4AGIOPUQLEIZKMGU"
+    secret_key = "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"
   }
 }
